2c3fa031d960316334809bda0f687af488f68331
simons <reggiesimons2cy@gmail.com>
2024-07-30 15:39:15 +0800

    update exam page

---
+++ frontend/src/app/exam/page.tsx
@@ -0,0 +1,15 @@
+import Developing from '@/src/components/Developing'
+import Exam from '@/src/components/exam/Rendering'
+
+export const metadata = {
+  title: 'Pink Mammoth',
+  description: 'we are web3 community',
+}
+
+export default function ExamTo() {
+  return (
+    <>
+      <Exam />
+    </>
+  )
+}
